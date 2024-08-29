4a2bf48411133e1bcb5dec895e87aca11f3188af
sai <ojiip95@gmail.com>
2024-08-29 22:43:43 +0200

    build: task 6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+""" Encrypting passwords """
 from typing import ByteString
 import bcrypt
 
@@ -7,3 +8,8 @@ def hash_password(password: str) -> ByteString:
     """ returns a salted, hashed password"""
     password = password.encode('utf-8')
     return bcrypt.hashpw(password, bcrypt.gensalt())
+
+
+def is_valid(hashed_password: ByteString, password: str):
+    """ check of a given password"""
+    return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
