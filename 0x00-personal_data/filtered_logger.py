cd2699c9758697d7cf0da0c1f96230be51b895ca
sai <ojiip95@gmail.com>
2024-08-29 18:22:44 +0200

    fix: code style

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,7 +4,9 @@ from typing import List
 import re
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(
+        fields: List[str], redaction: str,
+        message: str, separator: str) -> str:
     """returns the log message obfuscated
 
         Args:
@@ -18,15 +20,19 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
                 a string representing by which character is separating
                     all fields in the log line (message)
     """
-    
-    arr = message.split(separator)
+
+    list_of_strings: List[str] = message.split(separator)
     for field in fields:
         find_field = re.compile(f"(?:{field})=([^{separator}]+)")
         string_to_replace = find_field.search(message).group()
-        for idx, target in enumerate(arr):
+        for idx, target in enumerate(list_of_strings):
             if target == string_to_replace:
-                arr.pop(idx)
-                target = re.sub(f"(?:{field})=([^{separator}]+)", f"{field}={redaction}", target)
-                arr.insert(idx, target)
+                list_of_strings.pop(idx)
+                target = re.sub(
+                        f"(?:{field})=([^{separator}]+)",
+                        f"{field}={redaction}",
+                        target
+                        )
+                list_of_strings.insert(idx, target)
 
-    return f'{separator}'.join(arr)
+    return f'{separator}'.join(list_of_strings)
