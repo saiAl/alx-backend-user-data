bc9193845b63c95a9f1b820d196aeb410bbc7b59
sai <ojiip95@gmail.com>
2024-09-03 17:12:32 +0200

    refactor: Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,22 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """Basic authentication implementation.
     """
-    pass
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """Extracts the Base64-encoded authorization header
+            from a given string.
+
+            Args:
+                authorization_header: The authorization header string.
+
+            Returns:
+                The Base64-encoded portion of the authorization header,
+                    or None if the header is invalid.
+
+        """
+        return (
+                None if authorization_header is None
+                or not isinstance(authorization_header, str)
+                or authorization_header[:6] != 'Basic '
+                else authorization_header[6:]`:s
+                )
