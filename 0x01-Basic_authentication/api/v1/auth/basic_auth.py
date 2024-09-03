b39c123561894c0246340decdbfcf716f201494f
sai <ojiip95@gmail.com>
2024-09-03 17:02:05 +0200

    fix: add docstring to the module

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-""" """
+"""This module defines the `BasicAuth` class,
+    which is a subclass of the `Auth`
+        class from the `api.v1.auth.auth` module.
+"""
 from api.v1.auth.auth import Auth
 
+
 class BasicAuth(Auth):
-    """ """
+    """Basic authentication implementation.
+    """
     pass
