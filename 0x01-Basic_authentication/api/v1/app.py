ad9e360af6f54d51d6d96166fe4471b70253a8e5
sai <ojiip95@gmail.com>
2024-09-03 15:53:10 +0200

    fix: forbidden route

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -32,7 +32,7 @@ def unauthorized(error) -> str:
 def forbidden(error) -> str:
     """forbidden handler
     """
-    return jsonify({"error": "Forbidden"})
+    return jsonify({"error": "Forbidden"}), 403
 
 
 if __name__ == "__main__":
--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -35,7 +35,7 @@ def unauthorized():
 
 
 @app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
-def unauthorized():
+def forbidden():
     """GET /api/v1/forbidden
       raise a 403 error
     """
