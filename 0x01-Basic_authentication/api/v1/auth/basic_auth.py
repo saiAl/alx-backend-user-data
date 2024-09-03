88f9af6891fa92d8d5e810074c5d57d990076811
sai <ojiip95@gmail.com>
2024-09-03 22:15:33 +0200

    build: Basic - User object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,7 +6,8 @@
 from api.v1.auth.auth import Auth
 import base64
 import binascii
-
+from models.user import User
+from typing import TypeVar
 
 class BasicAuth(Auth):
     """Basic authentication implementation.
@@ -80,3 +81,20 @@ class BasicAuth(Auth):
                     decoded_base64_authorization_header.split(':')[-1]
                     )
                 )
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """ """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        
+        instance = None
+        try:
+            instance = User.search({user_email: user_pwd})
+        except Exception:
+            return None
+
+        return f"{instance.first_name} {instance.last_name}"
