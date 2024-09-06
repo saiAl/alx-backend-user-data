ce25ecbf80ad4b4b04e869be74fdf0ac5a33b0c4
sai <ojiip95@gmail.com>
2024-09-06 21:39:29 +0200

    fix: handle the None state of authorization header

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -2,11 +2,11 @@
 """
 Route module for the API
 """
-from os import getenv
-from api.v1.views import app_views
+
+import os
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
+from api.v1.views import app_views
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 
@@ -28,14 +28,13 @@ def handler():
             '/api/v1/status/',
             '/api/v1/unauthorized/',
             '/api/v1/forbidden/']
-    if auth is None:
-        return
-    if not auth.require_auth(request.path, allowed):
-        return
-    if auth.authorization_header(request) is None:
-        abort(401)
-    if auth.current_user(request) is None:
-        abort(403)
+    if auth is not None:
+        if not auth.require_auth(request.path, allowed):
+            pass
+        if auth.authorization_header(request) is None:
+            abort(401)
+        if auth.current_user(request) is None:
+            abort(403)
 
 
 @app.errorhandler(404)
@@ -60,6 +59,6 @@ def forbidden(error) -> str:
 
 
 if __name__ == "__main__":
-    host = getenv("API_HOST", "0.0.0.0")
-    port = getenv("API_PORT", "5000")
+    host = os.getenv("API_HOST", "0.0.0.0")
+    port = os.getenv("API_PORT", "5000")
     app.run(host=host, port=port, debug=True)
