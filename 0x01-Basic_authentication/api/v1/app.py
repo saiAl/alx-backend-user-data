a6f17d054b69dee3dd9610d04036c12d852e78a3
sai <ojiip95@gmail.com>
2024-09-06 21:54:11 +0200

    refactor: generate instance based on type of auth specified

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -14,9 +14,19 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
+auth_type = os.getenv("AUTH_TYPE")
 
-auth = Auth() if os.getenv("AUTH_TYPE") == 'auth' else None
-auth = BasicAuth() if os.getenv("AUTH_TYPE") == 'basic_auth' else None
+def factory(auth_t):
+    """generate auth instances
+    """
+    if auth_t == None:
+        return None
+    if auth_t == 'auth':
+        return Auth()
+    if auth_t == 'basic_auth':
+        return BasicAuth()
+
+auth = factory(auth_type)
 
 
 @app.before_request
