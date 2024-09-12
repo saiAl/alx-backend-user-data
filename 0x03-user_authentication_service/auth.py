5d427c4105f6e33273f6b166c506d663e17d409b
sai <ojiip95@gmail.com>
2024-09-12 09:09:27 +0200

    fix

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,12 +2,24 @@
 """Module for password hashing.
 """
 import bcrypt
+from db import DB
 
 
-def _hash_password(passwd):
+def _hash_password(password):
     """Hash a password using bcrypt.
     """
     return bcrypt.hashpw(
-            passwd.encode('utf-8'), 
+            password.encode('utf-8'),
             bcrypt.gensalt()
             )
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email, password):
+        pass
