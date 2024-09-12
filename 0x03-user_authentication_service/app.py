080d603741645af7d00910ab43d5e6eeeae00173
sai <ojiip95@gmail.com>
2024-09-12 10:12:29 +0200

    build: Register user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """Simple Flask web application.
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", strict_slashes=False)
@@ -14,6 +16,18 @@ def index():
         "message": "Bienvenue"
         })
 
+@app.route("/users", methods=['POST'], strict_slashes=False)
+def users():
+    """Registration users endpoint
+    """
+    try:
+        user = AUTH.register_user(
+                request.form.get("email"),
+                request.form.get("password")
+                )
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+    return jsonify({"email": user.email, "message": "user created"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port="5000", debug=True)
